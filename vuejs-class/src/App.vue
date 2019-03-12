04bff50546dd61d9c4dba891ff70d5886096667f
Cynthia Dickson <cynthiaiboro@gmail.com>
2019-03-12 11:39:18 +0100

    start hub fellowship vuejs class

---
+++ vuejs-class/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header';
+import Footer from './components/footer';
+export default {
+    components: {
+      Header,
+      Footer,
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ vuejs-class/src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>Copyright 2019 StartHub Fellowship</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    p {
+        text-align: center;
+        background: #141313;
+        color: #fcfcfc;
+        padding: 15px;
+    }
+</style>
